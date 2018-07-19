68bb811669b590a6a8f88df8592bb04887d28cf2
pszarka <p.szarka@mito.hu>
2018-07-19 15:55:39 +0200

    ListElement struct deleted

--- template/list.go
+++ template/list.go
@@ -12,7 +12,7 @@ const (
 type ListTemplate struct {
 	TopElementStyle string        `json:"top_element_style"`
 	Elements        []ListElement `json:"elements"`
-	Buttons         []ListButton  `json:"buttons,omitempty"`
+	Buttons         []Button      `json:"buttons,omitempty"`
 }
 
 func (ListTemplate) Type() TemplateType {
@@ -29,15 +29,6 @@ type ListElement struct {
 	Subtitle      string        `json:"subtitle,omitempty"`
 	DefaultAction DefaultAction `json:"default_action,omitempty"`
 	Buttons       []ListButton  `json:"buttons,omitempty"`
-}
-
-type ListButton struct {
-	Title               string `json:"title"`
-	Type                string `json:"type,omitempty"`
-	URL                 string `json:"url,omitempty"`
-	MessengerExtensions bool   `json:"messenger_extensions,omitempty"`
-	WebviewHeightRatio  string `json:"webview_height_ratio,omitempty"`
-	FallbackURL         string `json:"fallback_url,omitempty"`
 }
 
 func (l *ListTemplate) Decode(d json.RawMessage) error {
